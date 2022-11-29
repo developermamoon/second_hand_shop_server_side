2d36c40a3e7d01c914deca995b49604ecb0ae568
Md. Abdullah Al-Mamoon <almamoon18@gmail.com>
2022-11-30 00:10:47 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -8,12 +9,42 @@ app.use(cors());
 const allCars = require('./Data/allCars.json');
 const carCategories = require('./Data/CarCategories.json');
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.PASSWORD}@cluster0.dqmis3n.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+
+        const allCarsData = client.db('SellAnyCarServer').collection('AllCars');
+        
+        
+        // getting allCars data from the database
+        app.get('/allCars', async(req, res)=>{
+            const query = {};
+            const cursor = allCarsData.find(query);
+            const allCars = await cursor.toArray();
+            res.send(allCars);
+        })
+    }
+    finally{}
+}
+
+run().catch(err=>{
+    console.log(err);
+})
+
+
+
+
 app.get('/', (req, res) => {
     res.send('Dream Car server is Running');
 });
 
 
-app.get('/category', (req, res) =>{
+app.get('/categories', (req, res) =>{
     res.send(carCategories);
 })
 

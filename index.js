54e6bb77295912f70c9d91379e26962800544a65
Md. Abdullah Al-Mamoon <almamoon18@gmail.com>
2022-11-30 01:25:55 +0600

    category and allcategory fetch completed

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+
+const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+// middlewares
+app.use(cors())
+app.use(express.json())
 
-const allCars = require('./Data/allCars.json');
+// const allCars = require('./Data/allCars.json');
 const carCategories = require('./Data/CarCategories.json');
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.PASSWORD}@cluster0.dqmis3n.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dqmis3n.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -27,6 +32,25 @@ async function run(){
             const cursor = allCarsData.find(query);
             const allCars = await cursor.toArray();
             res.send(allCars);
+        })
+
+        // getting selected category cars data
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id;
+            if(id === '7'){
+                const query = { };
+                const cursor = allCarsData.find(query);
+                const selectedCategoryCarsData = await cursor.toArray();
+                res.send(selectedCategoryCarsData);
+            }
+            else{
+                const query = { category_id: id };
+                const cursor = allCarsData.find(query);
+                const selectedCategoryCarsData = await cursor.toArray();
+                res.send(selectedCategoryCarsData);
+                
+            }
+            
         })
     }
     finally{}
@@ -48,20 +72,20 @@ app.get('/categories', (req, res) =>{
     res.send(carCategories);
 })
 
-app.get('/allCars', (req, res)=>{
-    res.send(allCars);
-})
+// app.get('/allCars', (req, res)=>{
+//     res.send(allCars);
+// })
 
-app.get('/category/:id', (req, res)=>{
-    const id = req.params.id;
-    if(id === '7'){
-        res.send(allCars)
-    }
-    else{
-        const selectedCars = allCars.filter(n => n.category_id === id);
-        res.send(selectedCars);
-    }
-})
+// app.get('/category/:id', (req, res)=>{
+//     const id = req.params.id;
+//     if(id === '7'){
+//         res.send(allCars)
+//     }
+//     else{
+//         const selectedCars = allCars.filter(n => n.category_id === id);
+//         res.send(selectedCars);
+//     }
+// })
 
 app.listen(port, () => {
     console.log('Dream Car Server running on port', port);
